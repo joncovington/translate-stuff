f67d3c409a008d66d35a7ad902f6e1f67fe98ac5
Jon Covington <jon.covington@gmail.com>
2021-06-18 13:42:40 -0600

    Moved language selector to a component.

---
+++ src/components/LanguageSelector.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class LanguageSelector extends React.Component {
+    static contextType = LanguageContext;
+    render() { 
+        return (
+            <div>
+                Select a language:
+                <i className="flag us" onClick={() => this.props.onLanguageChange('english')}/>
+                <i className="flag nl" onClick={() => this.props.onLanguageChange('dutch')}/>
+            </div>
+        );
+    }
+};
+
+export default LanguageSelector;
