feb246c7db12744320bf963cd94fc99747bb8d4b
Jon Covington <jon.covington@gmail.com>
2021-06-18 13:38:09 -0600

    Initial commit. Used contextType for Field, Consumer for button.

---
+++ src/components/Button.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends React.Component {
+   
+    renderSubmit(value) {
+        return value === 'english' ? 'Submit' : 'Voorleggen'
+    }
+    render() {
+
+        return (
+            <div className="ui field">
+                <ColorContext.Consumer>
+                    {(color) => 
+                        <button className={`ui button ${color}`}>
+                            <LanguageContext.Consumer>
+                                {(value) => this.renderSubmit(value)}
+                            </LanguageContext.Consumer>
+                        </button>
+                    }
+                </ColorContext.Consumer>
+            </div>
+        );
+    };
+};
+
+export default Button;
